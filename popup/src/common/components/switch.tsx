0a06479b03312a74d62ace35eb7c7e0e14c842c6
kyo <sanghunjlee@gmail.com>
2024-04-15 08:07:30 -0700

    feat(popup): Add size & color & icon props to switch

--- popup/src/common/components/switch.tsx
+++ popup/src/common/components/switch.tsx
@@ -1,60 +1,127 @@
-import { MouseEvent, ComponentProps, useState, useCallback, ChangeEvent } from "react";
+import {
+  ComponentProps,
+  ChangeEvent,
+  ReactElement,
+  useCallback,
+  useMemo,
+  useId,
+} from "react";
 import useControlled from "@popup/src/common/utils/useControlled";
+import { ComponentColorType, ComponentSizeType } from "@popup/src/interface";
+import { getColorClass } from "../utils/colorHelper";
 
-interface SwitchProps extends Omit<ComponentProps<"div">,"onChange"> {
-    checked?: boolean;
-    onChange?: (newValue: boolean) => void;
+export interface SwitchProps extends Omit<ComponentProps<"div">, "onChange"> {
+  size?: ComponentSizeType;
+  color?: ComponentColorType;
+  checked?: boolean;
+  onChange?: (newValue: boolean) => void;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
 }
 
 export default function Switch(props: SwitchProps) {
-    const {
-        className, 
-        checked: checkedProp, 
-        onChange
-    } = props;
+  const {
+    className,
+    checked: checkedProp,
+    onChange,
+    size = "medium",
+    color = "default",
+    startIcon,
+    endIcon,
+    ...others
+  } = props;
 
-    const [checked, setCheckedState] = useControlled({controlled: checkedProp})
+  const id = useId();
+  const [checked, setCheckedState] = useControlled({ controlled: checkedProp });
 
-    const sizeClass = "w-[40px] h-[20px]"
-    const dotSizeClass = "w-[16px] h-[16px]"
+  const containerSizeClassMap: { [key in ComponentSizeType]: string } = {
+    small: "w-[36px] h-[18px]",
+    medium: "w-[56px] h-[28px]",
+    large: "w-[76px] h-[38px]",
+  };
 
-    const handleChange = function(event: ChangeEvent<HTMLInputElement>) {
+  const dotSizeClassMap: { [key in ComponentSizeType]: string } = {
+    small: "w-[16px] h-[16px]",
+    medium: "w-[26px] h-[26px]",
+    large: "w-[36px] h-[36px]",
+  };
 
-        const newChecked = event.target.checked;
-        
-        setCheckedState(newChecked);
+  const translateClassMap: { [key in ComponentSizeType]: string } = {
+    small: "translate-x-[17px]",
+    medium: "translate-x-[27px]",
+    large: "translate-x-[37px]",
+  };
 
-        if (onChange) {
-            onChange(newChecked);
-        }
+  const handleChange = function (event: ChangeEvent<HTMLInputElement>) {
+    const newChecked = event.target.checked;
+
+    setCheckedState(newChecked);
+
+    if (onChange) {
+      onChange(newChecked);
     }
+  };
+
+  const trackColorClass = useMemo(
+    () => getColorClass(checked ? color : "disabled"),
+    [checked]
+  );
+
+  const DotIcon = useCallback(() => {
+    const dotIconSizeMap: { [key in ComponentSizeType]: string } = {
+      small: "h-[14px]",
+      medium: "h-[24px]",
+      large: "h-[34px]",
+    };
 
     return (
-        <div className="flex items-center" >
-            <label>
-                <div 
-                    className={[
-                        sizeClass,
-                        "relative rounded-full border-2 border-gray-200 overflow-clip z-20 cursor-pointer",
-                        className,
-                    ].join(" ")}
-                >
-                    <div 
-                        className={[
-                            dotSizeClass,
-                            "absolute bg-white rounded-full transition duration-300 z-10",
-                            checked ? "translate-x-[20px]" : "",
-                        ].join(" ")}
-                    />
-                    <div 
-                        className={[
-                            "absolute inset-0",
-                            checked ? "bg-webtoon" : "bg-gray-300",
-                        ].join(" ")}
-                    />
-                </div>
-                <input type="checkbox" className="hidden" checked={checked} onChange={handleChange} />
-            </label>
+      <div
+        className={[
+          dotIconSizeMap[size],
+          "*:w-full *:h-full",
+          size === "small" ? "*:p-[1px]" : "*:p-1",
+        ].join(" ")}
+      >
+        {checked ? startIcon : endIcon}
+      </div>
+    );
+  }, [checked]);
+
+  return (
+    <div {...others} id={`switch-root-${id}`} className="flex items-center">
+      <label id={`switch-label-${id}`}>
+        <div
+          id={`switch-container-${id}`}
+          className={[
+            containerSizeClassMap[size],
+            "relative flex items-center rounded-full border-[2px] border-gray-200 overflow-clip z-20 cursor-pointer",
+            className,
+          ].join(" ")}
+        >
+          <div
+            id={`switch-dot-${id}`}
+            className={[
+              dotSizeClassMap[size],
+              "absolute bg-white rounded-full transition duration-300 z-10 overflow-clip",
+              "flex justify-center items-center",
+              checked ? translateClassMap[size] : "",
+            ].join(" ")}
+          >
+            <DotIcon />
+          </div>
+          <div
+            id={`switch-track-${id}`}
+            className={["absolute inset-0", trackColorClass].join(" ")}
+          />
         </div>
-    )
+        <input
+          id={`switch-input-${id}`}
+          type="checkbox"
+          className="hidden"
+          checked={checked}
+          onChange={handleChange}
+        />
+      </label>
+    </div>
+  );
 }
--- popup/src/interface.ts
+++ popup/src/interface.ts
@@ -1,8 +1,10 @@
 export interface ISetting {
-    iType: "setting";
-    key: string;
-    desc: string;
-    value: boolean;
-    toolTip?: string;
-
+  iType: "setting";
+  key: string;
+  desc: string;
+  value: boolean;
+  toolTip?: string;
 }
+
+export type ComponentSizeType = "small" | "medium" | "large";
+export type ComponentColorType = "default" | "disabled" | "gray" | "none";
