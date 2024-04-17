70703dec2952e3c072fa64844442547d2bf0133a
kyo <sanghunjlee@gmail.com>
2024-04-17 11:47:24 -0700

    chore: configs for mode transference

--- webpack.config.ts
+++ webpack.config.ts
@@ -13,72 +13,79 @@ const transform = (content: Buffer, _: string) => {
   return transformed;
 };
 
-const config: Configuration = {
-  context: resolve(__dirname, "src"),
-  entry: {
-    worker: "./worker.js",
-    webtoon: "./webtoon.js",
-    content: "./content.js",
-    incom: "./incom/index.tsx",
-  },
-  module: {
-    rules: [
-      {
-        test: /incom\\.+\.tsx?$/,
-        exclude: [/node_modules/],
-        use: [
+const config = function (env: any, argv: any): Configuration {
+  return {
+    mode: argv.mode ?? "development",
+    context: resolve(__dirname, "src"),
+    entry: {
+      worker: "./worker.js",
+      webtoon: "./webtoon.js",
+      content: "./content.js",
+      incom: "./incom/index.tsx",
+    },
+    module: {
+      rules: [
+        {
+          test: /incom\\.+\.tsx?$/,
+          exclude: [/node_modules/],
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: [/node_modules/],
+          use: ["babel-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugion.loader, "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+    resolve: {
+      alias: {
+        "@assets": resolve(__dirname, "src/assets"),
+        "@incom": resolve(__dirname, "src/incom"),
+      },
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    output: {
+      filename: "[name].js",
+      path: resolve(__dirname, "dist"),
+      // clean: true
+    },
+    devtool: false,
+    cache: true,
+    plugins: [
+      new MiniCssExtractPlugion({
+        filename: "./assets/[name].css",
+        // chunkFilename: "./assets/inject.css"
+      }),
+      new CopyPlugin({
+        patterns: [
           {
-            loader: "ts-loader",
-            options: {
-              transpileOnly: true,
+            from: "manifest.json",
+            to: "manifest.json",
+            transform,
+          },
+          {
+            from: "assets/*",
+            to: "[path][name][ext]",
+            globOptions: {
+              gitignore: true,
+              ignore: ["**/*.(js|css)"],
             },
           },
         ],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: [/node_modules/],
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugion.loader, "css-loader", "postcss-loader"],
-      },
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  output: {
-    filename: "[name].js",
-    path: resolve(__dirname, "dist"),
-    // clean: true
-  },
-  devtool: false,
-  cache: true,
-  plugins: [
-    new MiniCssExtractPlugion({
-      filename: "./assets/[name].css",
-      // chunkFilename: "./assets/inject.css"
-    }),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "manifest.json",
-          to: "manifest.json",
-          transform,
-        },
-        {
-          from: "assets/*",
-          to: "[path][name][ext]",
-          globOptions: {
-            gitignore: true,
-            ignore: ["**/*.(js|css)"],
-          },
-        },
-      ],
-    }),
-  ],
+  };
 };
 
 export default config;
