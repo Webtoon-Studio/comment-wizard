6c5ac6747ae52eaeebccca4ae2843b8e0f585f67
kyo <sanghunjlee@gmail.com>
2024-04-19 11:57:15 -0700

    chore: convert jest config to ES from commonJS

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   testEnvironment: "node", // Use 'node' environment for Jest tests
   setupFilesAfterEnv: ["./test/setup.js"], // Load setup file before running tests
 };
