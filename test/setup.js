31de5dc2a3b658b5593a49aa8556dfc4f684b02c
kyo <sanghunjlee@gmail.com>
2024-04-19 11:56:44 -0700

    test: Add cookie to the test env

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,5 @@
 const { JSDOM } = require("jsdom");
+const { TbUfo } = require("react-icons/tb");
 
 class LocalStorageMock {
   constructor() {
@@ -29,3 +30,8 @@ const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>");
 global.window = dom.window;
 global.document = dom.window.document;
 global.navigator = dom.window.navigator;
+
+Object.defineProperty(window.document, "cookie", {
+  writable: true,
+  value: "NEO_SES=something",
+});
