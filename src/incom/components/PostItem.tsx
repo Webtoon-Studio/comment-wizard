5dce15ac6c726fc552e179b506bf6cb7abbb25d1
kyo <sanghunjlee@gmail.com>
2024-04-22 21:26:11 -0700

    feat(incom): Implement buttons to postItems

--- src/incom/components/PostItem.tsx
+++ src/incom/components/PostItem.tsx
@@ -1,4 +1,9 @@
+import Button from "@root/src/incom/components/Button";
+import DeleteIcon from "@root/src/incom/components/DeleteIcon";
+import DislikeIcon from "@root/src/incom/components/DislikeIcon";
+import LikeIcon from "@root/src/incom/components/LikeIcon";
 import { Post } from "@root/src/webtoon";
+import { useId } from "react";
 
 interface PostItemProps {
   post: Post;
@@ -6,6 +11,7 @@ interface PostItemProps {
 
 export default function PostItem(props: PostItemProps) {
   const { post } = props;
+  const id = useId();
   return (
     <div>
       <div>
@@ -19,8 +25,6 @@ export default function PostItem(props: PostItemProps) {
       <div>
         <div className="my-2 text-lg">
           <p>{post.body}</p>
-        </div>
-        <div>
           {post.createdAt ? (
             <span className="text-sm text-gray-400">
               {new Date(post.createdAt).toLocaleDateString(undefined, {
@@ -30,6 +34,23 @@ export default function PostItem(props: PostItemProps) {
               })}
             </span>
           ) : null}
+        </div>
+        <div className="w-full flex justify-end gap-2">
+          <Button id={`${id}like-button`}>
+            <div className="h-[23px] flex items-center">
+              <LikeIcon />
+              <span>{post.likes}</span>
+            </div>
+          </Button>
+          <Button id={`${id}dislike-button`}>
+            <div className="h-[23px] flex items-center">
+              <DislikeIcon />
+              <span>{post.dislikes}</span>
+            </div>
+          </Button>
+          <Button id={`${id}delete-button`}>
+            <DeleteIcon />
+          </Button>
         </div>
       </div>
     </div>
