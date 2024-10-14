f04d8d70a55a30995d47423f622338213e8ecad7
kyo <sanghunjlee@gmail.com>
2024-10-14 11:13:50 -0700

    feat: Implement props for button

--- src/features/incom/features/components/Button.tsx
+++ src/features/incom/features/components/Button.tsx
@@ -1,14 +1,71 @@
-import type { ComponentProps } from "react";
+import type { ComponentRoundedType, ComponentSizeType } from "@shared/interface/componentInterface";
+import { useMemo, type ComponentProps, type HTMLAttributes } from "react";
 
-interface ButtonProps extends ComponentProps<"button"> {}
+interface ButtonProps extends ComponentProps<"button"> {
+	variant?: "text" | "contained" | "outlined";
+	size?: ComponentSizeType;
+	rounded?: ComponentRoundedType;
+}
 
 export default function Button(props: ButtonProps) {
-	const { className, children, ...others } = props;
+	const { 
+		type = "button",
+		variant = "text",
+		size = "md",
+		rounded = "md",
+
+		className, 
+		children, 
+		...others 
+	} = props;
+
+	const hoverClassName = useMemo(() => {
+		switch (variant) {
+			case "text":
+				return "";
+			case "contained":
+				return "";
+			case "outlined":
+				return "";
+		}
+	}, [variant]);
+
+	const variantClassName = useMemo(() => {
+		switch (variant) {
+			case "text":
+				return "";
+			case "contained":
+				return "";
+			case "outlined":
+				return "border-[0.5px] border-solid border-[#d3d3d3]";
+		}
+	}, [variant])
+	
+	const sizeClassName = useMemo(() => {
+		switch (size) {
+			case "sm":
+				return "text-[10px] h-[18px] leading-[16px] px-[6px]";
+			case "md":
+				return "text-[14px] h-[33px] leading-[21px] px-[9.5px]";
+			case "lg":
+				return "text-[18px] h-[40px] leading-[28px] px-[14px]";
+		}
+	}, [size]);
+
+	const roundedClassName = useMemo(() => {
+		return `rounded-${rounded}`;
+	}, [rounded]);
+
 	return (
 		<button
+			type={type}
 			className={[
 				className,
-				"flex items-center shrink-0 text-[#b2b2b2] border-[0.5px] border-solid border-[#d3d3d3] rounded-[2px] cursor-pointer text-[14px] h-[33px] leading-[21px] px-[9.5px]",
+				"flex items-center shrink-0 cursor-pointer ",
+				hoverClassName,
+				variantClassName,
+				sizeClassName,
+				roundedClassName,
 			].join(" ")}
 			{...others}
 		>
---
+++ src/shared/interface/componentInterface.ts
@@ -0,0 +1,3 @@
+export type ComponentSizeType = "sm" | "md" | "lg";
+
+export type ComponentRoundedType = "none" | "sm" | "md" | "lg" | "full";
