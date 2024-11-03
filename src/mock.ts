91d66238ff2ea9e0b2da940e08df75e9e5dc587d
kyo <sanghunjlee@gmail.com>
2024-11-03 01:32:21 -0800

    Fix: mock post data generation

--- src/mock.ts
+++ src/mock.ts
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
 import type { EpisodeItem, SeriesItem } from "@shared/global";
-import { type PageIdType, Post, type PostIdType } from "@shared/post";
+import { type IPost, type PageIdType, Post, type PostIdType } from "@shared/post";
 
 export function mockSeriesItem(): SeriesItem {
 	if (!faker) {
@@ -74,9 +74,79 @@ export function mockEpisodeItems(titleId: `${number}`, page: number): EpisodeIte
 }
 
 
-export function mockPostData(): Post {
+export function mockPostData(): IPost {
 	if (!faker) {
-		return new Post();
+		const likeCount = 500 - Math.floor(Math.random() * 1000);
+		return {
+			serviceTicketId: "epicom",
+			pageId: "w_1320_276" as PageIdType, // e.g. "w_1320_276"
+			pageUrl: "_w_1320_276" as `_${PageIdType}`, // e.g. "_w_1320_276"
+			isOwner: false,
+			isPinned: false,
+			commentDepth: 1,
+			depth: 1,
+			creationType: "BY_USER", // e.g. "BY_USER" // TODO: Specify more?
+			status: "SERVICE", // e.g. "SERVICE" // TODO: Specify more?
+			body: "This is a test message",
+			bodyFormat: {
+				type: "PLAIN", // e.g. "PLAIN" // TODO: Specify more?
+				version: "", // e.g. ""
+			},
+			settings: {
+				reply: "ON", // Guessing the type here. Revisit if error
+				reaction: "OFF",
+				spoilerFilter: "OFF",
+			},
+			sectionGroup: {
+				totalCount: 0,
+				sections: [], // TODO: Specify type of the array?
+			},
+			reactions: [
+				{
+					reactionId: "post_like", // e.g. "post_like" // TODO: Specify more?
+					contentId: "GW-epicom:0-w_1320_276-ee", // e.g. "GW-epicom:0-w_1320_276-ee"
+					emotions: [
+						{
+							emotionId: "like", //e.g. "like" // TODO: Specify more?
+							count: Math.max(0, likeCount),
+							reacted: likeCount > 0,
+						},
+						{
+							emotionId: "dislike", //e.g. "like" // TODO: Specify more?
+							count: Math.min(0, likeCount),
+							reacted: likeCount < 0,
+						},
+					],
+				},
+			],
+			extraList: [], // TODO: Specify type of the array?
+			createdBy: {
+				publisherType: "PAGE", // e.g. "PAGE" // TODO: Specify more?
+				id: "mock_username", // e.g. "wbi2v3"
+				status: "SERVICe", // e.g. "SERVICE" // TODO: Specify more?
+				cuid: crypto.randomUUID(), // e.g. "2f586b40-5a1c-11eb-9de0-246e96398d40"
+				name: "MockUserName", // e.g. "Gr33nEclipse"
+				profileUrl: `_mock_username`, // e.g. "_wbi2v3"
+				isCreator: Math.random() > 0.5, // Poster is a creator
+				isPageOwner: false, // Poster is the creator of the series this post is in
+				maskedUserId: "mock*********", // e.g. "2f58****"
+				encUserId: "", // e.g. ""
+				profileImage: {}, // TODO: Specify more. Currently, not sure of the shape of this object
+				extraList: [], // TODO: Specify type of the array?
+				restriction: {
+					isWritePostRestricted: false,
+					isBlindPostRestricted: false,
+				},
+			},
+			createdAt: new Date().getTime(), // e.g. 1712368102285
+			updatedAt: new Date().getTime(), // e.g. 1712368102285
+			childPostCount: Math.floor(Math.random() * 10),
+			activeChildPostCount: Math.floor(Math.random() * 10),
+			pageOwnerChildPostCount: Math.floor(Math.random() * 10),
+			activePageOwnerChildPostCount: Math.floor(Math.random() * 10),
+			id: "GW-epicom:0-w_1320_276-ee", // e.g. "GW-epicom:0-w_1320_276-ee"
+			rootId: "GW-epicom:0-w_1320_276-ee", // e.g. "GW-epicom:0-w_1320_276-ee"
+		} satisfies IPost;
 	}
 	const pageId = `c_109098_${faker.number.int({ min: 1, max: 200 })}`;
 	const contentId = `GW-epicom:0-${pageId}-${faker.number
@@ -88,8 +158,9 @@ export function mockPostData(): Post {
 	const username = faker.internet.userName();
 
 	const createdDate = faker.date.past();
+	const modifiedDate = faker.number.int() % 10 === 0 ? faker.date.between({from: createdDate, to:new Date()}) : createdDate;
 
-	return new Post({
+	return {
 		serviceTicketId: "epicom",
 		pageId: pageId as PageIdType, // e.g. "w_1320_276"
 		pageUrl: `_${pageId}` as `_${PageIdType}`, // e.g. "_w_1320_276"
@@ -98,7 +169,7 @@ export function mockPostData(): Post {
 		commentDepth: 1,
 		depth: 1,
 		creationType: "BY_USER", // e.g. "BY_USER" // TODO: Specify more?
-		status: "SERVICE", // e.g. "SERVICE" // TODO: Specify more?
+		status: faker.number.int() % 10 === 0 ? "DELETE" : "SERVICE", // e.g. "SERVICE" // TODO: Specify more?
 		body: faker.lorem.lines(),
 		bodyFormat: {
 			type: "PLAIN", // e.g. "PLAIN" // TODO: Specify more?
@@ -135,11 +206,11 @@ export function mockPostData(): Post {
 		createdBy: {
 			publisherType: "PAGE", // e.g. "PAGE" // TODO: Specify more?
 			id: username, // e.g. "wbi2v3"
-			status: "SERVICe", // e.g. "SERVICE" // TODO: Specify more?
+			status: "SERVICE", // e.g. "SERVICE" // TODO: Specify more?
 			cuid: crypto.randomUUID(), // e.g. "2f586b40-5a1c-11eb-9de0-246e96398d40"
 			name: faker.internet.displayName(), // e.g. "Gr33nEclipse"
 			profileUrl: `_${username}`, // e.g. "_wbi2v3"
-			isCreator: faker.number.int() % 2 === 0, // Poster is a creator
+			isCreator: faker.number.int() % 6 === 0, // Poster is a creator
 			isPageOwner: false, // Poster is the creator of the series this post is in
 			maskedUserId:
 				username.length > 4
@@ -154,12 +225,12 @@ export function mockPostData(): Post {
 			},
 		},
 		createdAt: createdDate.getTime(), // e.g. 1712368102285
-		updatedAt: createdDate.getTime(), // e.g. 1712368102285
+		updatedAt: modifiedDate.getTime(), // e.g. 1712368102285
 		childPostCount: faker.number.int({ min: 0, max: 200 }),
 		activeChildPostCount: faker.number.int({ min: 0, max: 200 }),
 		pageOwnerChildPostCount: faker.number.int({ min: 0, max: 200 }),
 		activePageOwnerChildPostCount: faker.number.int({ min: 0, max: 200 }),
 		id: contentId, // e.g. "GW-epicom:0-w_1320_276-ee"
 		rootId: contentId, // e.g. "GW-epicom:0-w_1320_276-ee"
-	});
+	} satisfies IPost;
 }
